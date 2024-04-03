cae8198c1f1ba1c560b6a852d7d309a7320c155d
Jonathan Campana <jonathan.campana@mail.mcgill.ca>
2024-04-03 16:52:07 -0400

    Started coding algo 2

--- Matlab_code/+DeblurStuff/+algorithms/DRPD.m
+++ Matlab_code/+DeblurStuff/+algorithms/DRPD.m
@@ -2,6 +2,30 @@
 %%%%%%%%%%%%%%
 % Algorithm 2: douglasrachfordprimaldual
 %%%%%%%%%%%%%%
+function algo = DRPD()
+    algo.initialize =@(xinit, b, kernel, i) drfpd_init( xinit, b, kernel, i );
+    algo.iterate =@(ivals, b, kernel, i, problem ) drfpd_run( ivals, b, kernel, i, problem );
+end
+
+
+function ivals = drfpd_init( xinit, b, kernel, i )
+
+    if ~isfield(i, 'gammal1'), i.gammal1 = 0.049; end
+    if ~isfield(i, 'tprimaldualdr'), i.tprimaldualdr = 0.5 ; end
+    if ~isfield(i, 'rhoprimaldualdr'), i.rhoprimaldualdr = 0.05; end
+    
+    apply = DeblurStuff.utilities.multiplyingmatrix(b, kernel);
+    ivals.z1 = xinit;
+    %ivals.z1 = b;
+    ivals.z2 = cat(3, apply.K(xinit), apply.D(xinit));
+    %dim = size(b);
+    %ivals.z2 = zeros(dim(1), dim(2), 3);
+end
+
+
+
+
+
 
 
 
