cc122387da3aed932168c8063d1af0267324ced2
Jonathan Campana <jonathan.campana@mail.mcgill.ca>
2024-04-04 19:25:11 -0400

    getting somewhere in algo 2

---
+++ Matlab_code/+DeblurStuff/+utilities/+prox/conjugateprox.m
@@ -0,0 +1,13 @@
+%Calculating the conjugate of the prox
+function output = conjugateprox(prox, y, t)
+    arguments
+        prox function_handle
+        y (:,:,3) double
+        t double
+    end
+
+    %Apply Moreau decomposition theorem (as shown in the report).
+    %We have two inputs as we use it on the isonorm.
+    output = y - t*prox(y/t, 1/t);
+end
+
