2c5a75528ba499f7bd7f3fab36bb1f0b5e34fd0b
Elliot Forcier-Poirier <elliot.forcier-poirier@mail.mcgill.ca>
2024-04-13 23:31:50 -0400

    Implemented comprehensive grid search for Douglas Rachford Primal

---
+++ Matlab_code/+DeblurStuff/+utilities/comprehensive_grid_search.asv
@@ -0,0 +1,76 @@
+% grid searching for all the algorithms
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Initialize default parameters %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+I = imread('+DeblurStuff/+image_files/cameraman.jpg');
+I = rgb2gray(I);
+
+% Resize so each pixel is in between 0 and 1
+I = double(I(:, :, 1));
+mn = min(I(:));
+I = I - mn;
+mx = max(I(:));
+I = I/mx;
+
+% default i params
+i.verbose = 0;
+i.malength = 32;
+i.maxiter = 200;
+i.gammal1 = 0.1;
+i.gammal2 = 0.02;
+i.tprimaldr = 2;
+i.rhoprimaldr = 1.4;
+i.tprimaldualdr = 0.8;
+i.rhoprimaldualdr = 0.1;
+i.tadmm = 2.2;
+i.rhoadmm = 1.5;
+i.scp = 0.2; 
+i.tcp = 0.2;
+
+kernelsize = 10;
+% Blurring
+%[kernel,b] = DeblurStuff.image_handling.GaussianBlur(I,kernelsize,2);
+[kernel, b] = DeblurStuff.image_handling.MotionBlur(I,kernelsize,0);
+
+% Additive noise
+b = DeblurStuff.image_handling.SaltnPepper(b, 0.01);
+
+
+% DRP grid_searching
+array_gamma = [0.025, 0.05, 0.075, 0.1, 0.125, 0.15];
+array_rho = [0.1, 0.25, 0.5, 1, 1.5, 2];
+array_t = [0.1, 0.25, 0.5, 1, 1.5, 2];
+
+
+% Assuming previous initialization and parameter setting is done correctly
+
+for j = array_rho
+    i.rhoprimaldr = j;
+    for k = array_t
+        i.tprimaldr = k;
+
+        % Grid search for l1
+        error_array = DeblurStuff.utilities.grid_search('douglasrachfordprimal', i, 'gammal1', array_gamma, b, kernel, 'l1');
+        fig = figure;
+        plot(array_gamma, error_array);
+        title(sprintf('L1 Error with rho=%.2f, t=%.2f', j, k));
+        xlabel('Gamma');
+        ylabel('Error');
+        grid on;
+        saveas(fig, sprintf('+DeblurStuff/+results/L1_Error_rho%.2f_t%.2f.png', j, k));
+
+        % Grid search for l2
+        error_array = DeblurStuff.utilities.grid_search('douglasrachfordprimal', i, 'gammal2', array_gamma, b, kernel, 'l2');
+        fig = figure;
+        plot(array_gamma, error_array);
+        title(sprintf('L2 Error with rho=%.2f, t=%.2f', j, k));
+        xlabel('Gamma');
+        ylabel('Error');
+        grid on;
+        saveas(fig, sprintf('+DeblurStuff/+results/L2_Error_rho%.2f_t%.2f.png', j, k));
+
+    end
+end
+
--- Matlab_code/+DeblurStuff/+utilities/grid_search.m
+++ Matlab_code/+DeblurStuff/+utilities/grid_search.m
@@ -5,32 +5,39 @@ function array_y = grid_search(algorithm, i, test_param, array_of_params, b, ker
 %   Input Parameters:
 %       algorithm       : which of the algorithm
 %       i               : all the other parameters being fixed
+%       array_of_params : the various values of the test parameter to be
+%       tes
 %       test_parameter  : which parameter is fixed
 %       b               : Blurred image
 %       problem         : l1 or l2, for the norm type
 %   Output Parameters:
 %       array_y         : performance of the different values of the parameter being tested
 %
-    array_y = zeros(1, length(array_of_params)); % Initialize array of errors
-    ivals = init_algo(algorithm, i, b, kernel); % Initialize algorithm values
+    array_y = zeros(length(array_of_params)); % Initialize array of errors
+    [numRows, numCols] = size(b);
+    x = zeros( numRows, numCols );
     
     for j = 1:length(array_of_params)
         i.(test_param) = array_of_params(j); % Dynamically set the test parameter
         
         switch lower(algorithm) % Choose the correct algorithm
             case 'douglasrachfordprimal'
+                [ivals, i] = DeblurStuff.init.init_algo('douglasrachfordprimal', i, b, x, kernel );
                 [~, summary] = DeblurStuff.algorithms.DRP(ivals, b, i, problem);
                 array_y(j) = summary.e;
                 
             case 'douglasrachfordprimaldual'
+                [ivals, i] = DeblurStuff.init.init_algo('douglasrachfordprimaldual', i, b, x, kernel );
                 [~, summary] = DeblurStuff.algorithms.DRPD(ivals, b, i, problem);
                 array_y(j) = summary.e;
                 
             case 'admm'
+                [ivals, i] = DeblurStuff.init.init_algo('admm', i, b, x, kernel );
                 [~, summary] = DeblurStuff.algorithms.ADMM(ivals, b, i, problem);
                 array_y(j) = summary.e;
                 
             case 'chambollepock'
+                [ivals, i] = DeblurStuff.init.init_algo('chambollepock', i, b, x, kernel );
                 [~, summary] = DeblurStuff.algorithms.CP(ivals, b, i, problem);
                 array_y(j) = summary.e;
                 
