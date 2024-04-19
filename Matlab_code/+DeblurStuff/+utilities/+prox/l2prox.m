ff2bfcd209bc1ffe222391b9e4fbc7874affffef
Edmund Wu <edmundzjwu@gmail.com>
2024-04-19 09:40:02 -0400

    l2 norm details

--- Matlab_code/+DeblurStuff/+utilities/+prox/l2prox.m
+++ Matlab_code/+DeblurStuff/+utilities/+prox/l2prox.m
@@ -1,5 +1,5 @@
 % returns the l2 prox of (y-b)
 % Note: this function assumes the input parameters are correct.
 function [ sol ] = l2prox( y, b, lambda )
-    sol = (y + 2*lambda*b) ./ (2*lambda + 1);
+    sol = (y + 2*lambda*b) / (2*lambda + 1);
 end
--- Matlab_code/+DeblurStuff/main.m
+++ Matlab_code/+DeblurStuff/main.m
@@ -56,7 +56,7 @@ i.verbose = 0;
 i.malength = 32;
 i.maxiter = 100;
 i.gammal1 = 0.1;
-i.gammal2 = 0.5;
+i.gammal2 = 0.01;
 i.tprimaldr = 2;
 i.rhoprimaldr = 1.4;
 i.tprimaldualdr = 0.8;
@@ -64,13 +64,13 @@ i.rhoprimaldualdr = 0.1;
 i.tadmm = 2.2;
 i.rhoadmm = 1.5;
 i.scp = 0.2; 
-i.tcp = 0.1;
+i.tcp = 0.2;
 [numRows, numCols] = size(b);
 x = zeros( numRows, numCols );
 % x = DeblurStuff.DeblurGod('l1', 'douglasrachfordprimal', x, kernel, b, i);
 x = DeblurStuff.DeblurGod('l2', 'douglasrachfordprimaldual', x, kernel, b, i);
 % x = DeblurStuff.DeblurGod('l2', 'admm', x, kernel, b, i);
-% x = DeblurStuff.DeblurGod('l2', 'chambollepock', x, kernel, b, i);
+% x = DeblurStuff.DeblurGod('l1', 'chambollepock', x, kernel, b, i);
 
 
 figure('Name','image after deblurring')
